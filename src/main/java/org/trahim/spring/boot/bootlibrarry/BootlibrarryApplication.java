b73136e2145d622894a932ce1ae4bbd65a793c58
trahim13 <trachimovich@gmail.com>
2018-07-23 22:57:34 +0300

    Initial commit

---
+++ src/main/java/org/trahim/spring/boot/bootlibrarry/BootlibrarryApplication.java
@@ -0,0 +1,12 @@
+package org.trahim.spring.boot.bootlibrarry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootlibrarryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootlibrarryApplication.class, args);
+    }
+}
